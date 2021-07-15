3d28eb9c33d314aae994ebde43069e7297a6f373
amrit04092000 <amrit04092000@gmail.com>
2021-07-15 21:56:21 +0530

    page complete

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(imgsrc){
+    document.querySelector('.starbucks').src = imgsrc;
+}
+function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
